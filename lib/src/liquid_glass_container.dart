cc4ed7e926b225f432deb26f3488b0d42bd3ba62
UTSO SARKAR <62977856+officiallyutso@users.noreply.github.com>
2025-07-20 23:25:11 +0530

    feat: basic container added

---
+++ lib/src/liquid_glass_container.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'liquid_glass_widget.dart';
+import 'config.dart';
+
+/// A container widget with liquid glass effect
+class LiquidGlassContainer extends StatelessWidget {
+  final Widget child;
+  final double? width;
+  final double? height;
+  final double blurSigma;
+  final double distortionIntensity;
+  final double animationSpeed;
+  final Color glassColor;
+  final BorderRadius? borderRadius;
+  final EdgeInsetsGeometry? padding;
+  final EdgeInsetsGeometry? margin;
+  final bool useBackdrop;
+  
+  ///enablef touch ripple effects
+  final bool enableTouchRipples;
+  
+  const LiquidGlassContainer({
+    Key? key,
+    required this.child,
+    this.width,
+    this.height,
+    this.blurSigma = 20.0,
+    this.distortionIntensity = 0.04,
+    this.animationSpeed = 1.5,
+    this.glassColor = Colors.white54,
+    this.borderRadius,
+    this.padding,
+    this.margin,
+    this.useBackdrop = true,
+    this.enableTouchRipples = false,
+  }) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    return LiquidGlassGLSL(
+      width: width,
+      height: height,
+      padding: padding,
+      margin: margin,
+      config: LiquidGlassConfig(
+        blurSigma: blurSigma,
+        distortionIntensity: distortionIntensity,
+        animationSpeed: animationSpeed,
+        glassColor: glassColor,
+        borderRadius: borderRadius,
+        useBackdrop: useBackdrop,
+        enableTouchRipples: enableTouchRipples,
+      ),
+      child: child,
+    );
+  }
+}
